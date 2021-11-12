5d5f496b178594fd3086f73df36f325183da5fbe
Pranav Goli <94185588+Zeta-Ray@users.noreply.github.com>
2021-11-12 11:38:21 -0500

    Add files via upload

---
+++ hello_world.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<string>
+using std::cout; 
+using std::cin;
+using std::string;
+using std::endl;
+
+int main(){
+  string x;
+  cin >> x; 
+  
+  int y;
+  cin >> y;
+  
+  string z;
+  cin >> z;
+  
+  cout << z << endl;
+  cout << x << endl;
+  cout << y << endl;
+}
